e78aaa7faff5edf69342701fc4a840047e99afb4
Patrick McIsaac <patrick.mcisaac.dev@gmail.com>
2025-09-07 15:56:39 -0500

    getting started

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native"
+
+export default function Index() {
+    return (
+        <View className="flex-1 items-center justify-center">
+            <Text className="text-center text-5xl font-bold text-dark-100">
+                Welcome!
+            </Text>
+        </View>
+    )
+}
